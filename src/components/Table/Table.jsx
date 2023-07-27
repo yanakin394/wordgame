8cd91d3594e35346419a6ad3aa1eedfc6983c050
yanakin394 <yana.germatskaya@gmail.com>
2023-07-27 13:01:01 +0300

    table edit/del buttons and styles added

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,16 +1,32 @@
 import wordsArr from '../data/words.json';
 import style from './Table.module.scss';
+import editBtn from '../../assets/icons/edit.png';
+import deleteBtn from '../../assets/icons/delete.png';
 
 function Table() {
     return (
         <table className={style.table}>
-            <caption>Word List</caption>
-            <tr>
-                <th className={style.td}>English</th>
-                <th className={style.td}>Transcription</th>
-                <th className={style.td}>Translation</th>
-                <th className={style.td}>Edit/Delete</th>
-            </tr>
+            <caption className={style.table__title}>Word List</caption>
+            <thead>
+                <tr>
+                    <th className={style.col_title}>English</th>
+                    <th className={style.col_title}>Transcription</th>
+                    <th className={style.col_title}>Translation</th>
+                    <th className={style.col_title}>Edit/Delete</th>
+                </tr>
+            </thead>
+            <tbody> {wordsArr.map((item, index) => (
+                <tr key={index}>
+                    <th className={style.td}>{item.english}</th>
+                    <th className={style.td}>{item.transcription}</th>
+                    <th className={style.td}>{item.russian}</th>
+                    <th className={style.td}>
+                        <img src={editBtn} alt="edit" className={style.edit__btn}/>
+                        <img src={deleteBtn} alt="button" className={style.del__btn}/>
+                    </th>
+                </tr>
+                ))}
+            </tbody>
         </table>
     );
 }
