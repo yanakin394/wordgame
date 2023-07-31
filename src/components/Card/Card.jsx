1a4140e3d37dd2209585eab480ac41b4e8da3dc1
yanakin394 <yana.germatskaya@gmail.com>
2023-07-31 13:12:23 +0300

    conditional rendering to the card button added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,10 +5,8 @@ import style from './Card.module.scss';
 export default function Card() {
     const [clicked, setClicked] = useState(false);
     const handleClick = () => {
-        setClicked();
-    }
-    {
-        if (clicked === false) {
+        setClicked(!clicked);
+                }
             return (        
                 <div className={style.card}>
                     <div className={style.card__wrapper}>
@@ -16,22 +14,8 @@ export default function Card() {
                             <p className={style.card__word}>{wordsArr[0].english}</p>
                             <p className={style.card__transcription}>{wordsArr[0].transcription}</p>
                         </div>
-                        <button className={style.card__button} onClick={() => handleClick}>Проверить</button>                        
+                        {clicked ? <p className={style.card__translation}>{wordsArr[0].russian}</p> : <button className={style.card__button} onClick={handleClick}>Проверить</button>}                     
                     </div>
                 </div>       
             );
-        } else {   
-            return (        
-                <div className={style.card}>
-                    <div className={style.card__wrapper}>
-                        <div className={style.card__word}>
-                            <p className={style.card__word}>{wordsArr[0].english}</p>
-                            <p className={style.card__transcription}>{wordsArr[0].transcription}</p>
-                        </div>
-                        <p className={style.card__translation}>{wordsArr[0].russian}</p>
-                    </div>
-                </div>       
-            );
-        } 
-    }
 }
